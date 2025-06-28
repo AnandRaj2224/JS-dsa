83ed67bfac053bf590c6fbb68502078a5a5488d1
Anand <anandraj6783@gmail.com>
2025-06-28 21:17:38 +0530

    solved question 3

--- Recursion.js
+++ Recursion.js
@@ -16,4 +16,15 @@ function power(base,expo) {
   if(expo === 0) return 1;
   return base * power(base,expo-1);
 }
-console.log(power(2,3));
+// console.log(power(2,3));
+
+/*
+Write a function that returns the factorial of a number.
+ As a quick refresher, a factorial of a number is the result of that number multiplied by the number before it, and the number before that number, and so on, until you reach 1. The factorial of 1 is just 1.
+*/
+
+function fact(n) {
+  if(n === 1) return 1;
+  return n * fact(n-1);
+}
+// console.log(fact(5));
