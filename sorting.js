7cb5df3b91adc4bd3263a33e57fae6d656b1b9c7
Anand <anandraj6783@gmail.com>
2025-07-03 22:17:53 +0530

    my implementation of selection sort

--- sorting.js
+++ sorting.js
@@ -21,4 +21,22 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log(bubbleSort([9,2,3,4,5,6]));
+// console.log(bubbleSort([4,2,1,7,5,6]));
+
+
+// my implementation of selection sort 
+
+function selectionSort(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let smallest = i;
+    for(let j = 1+i; j < arr.length; j++) {
+      if(arr[smallest] > arr[j]) {
+        smallest = j;
+      }
+    }
+    swap(arr,i,smallest);
+  }
+  return arr;
+}
+
+// console.log(selectionSort([4,2,1,3,5,6]));
