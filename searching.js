13ea7f7ab514db1ead68ae849bdf78b43c3ffd5b
Anand <anandraj6783@gmail.com>
2025-07-03 13:03:38 +0530

    my implementation of binary search

--- searching.js
+++ searching.js
@@ -10,4 +10,31 @@ function linearSearch (value,target) {
    return -1;
 }
 
-console.log(linearSearch([1,1,1,1,22,2,3,4,5,5,5],22));
+// console.log(linearSearch([1,1,1,1,22,2,3,4,5,5,5],22));
+
+// my implementation of binary search
+
+function binarySearch (value,target) {
+
+  let left = 0;
+  let right = value.length-1;
+
+  while (left <= right) {
+
+    let mid = Math.floor((left+right/2));
+
+    if(value[mid] === target) {
+      return mid;
+    }
+
+    if(value[mid] > target) {
+      left = mid+1;
+    } else {
+      right = mid-1;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([2,3,4,5,6,7,8],5));
