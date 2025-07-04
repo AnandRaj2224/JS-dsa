e2cd76db4e9f2d1e553e7405a49dfb134d420195
Anand <anandraj6783@gmail.com>
2025-07-04 14:04:50 +0530

    create function for merge sort recursively

--- Project_Recursion.js
+++ Project_Recursion.js
@@ -25,4 +25,46 @@ function recursiveFibo(num) {
   return arr;
 }
 
-console.log(recursiveFibo(8));
+ // console.log(recursiveFibo(8));
+
+
+
+ // merge sort
+
+function mergeArr(arr1,arr2) {
+  let mergedArr = [];
+  let i = 0;
+  let j = 0;
+    while( i < arr1.length && j < arr2.length) {
+      if(arr1[i] > arr2[j]) {
+        mergedArr.push(arr2[j]);
+        j++;
+      } else {
+        mergedArr.push(arr1[i]);
+        i++;
+      }
+    }
+
+    while(i < arr1.length) {
+      mergedArr.push(arr1[i]);
+      i++;
+    }
+
+    while(j < arr2.length) {
+      mergedArr.push(arr2[j]);
+      j++;
+    }
+
+  return mergedArr;
+}
+
+function mergeSort(array) {
+
+  if(array.length === 1 || array.length === 0 ) return array;
+  let mid = Math.floor(array.length/2);
+  let left = mergeSort(array.slice(0,mid));
+  let right = mergeSort(array.slice(mid));
+  return mergeArr(left,right);
+}
+
+console.log(mergeSort([1,2,4,5,9,11,17,26,27,3,6,7,8,25,24,23,12,10]));
