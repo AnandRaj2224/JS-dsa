8f46c55d9c75e75e11081b80f8b16faf07a21f2b
Anand <anandraj6783@gmail.com>
2025-07-10 14:56:20 +0530

    implemented method clear

--- Project_HashMap.js
+++ Project_HashMap.js
@@ -10,45 +10,48 @@ class HashMap {
     }
     return hashCode;
   }
-  set(key,value) {
+  set(key, value) {
     let index = this._hash(key);
-    if(!this.bucket[index]) {
+    if (!this.bucket[index]) {
       this.bucket[index] = [];
     }
-    this.bucket[index].push([key,value]);
+    this.bucket[index].push([key, value]);
   }
   get(key) {
     let index = this._hash(key);
-    if(!this.bucket[index]) return null;
-    for(let i = 0; i < this.bucket[index].length; i++) {
-      if(this.bucket[index][i][0] === key) return this.bucket[index][i][1];
+    if (!this.bucket[index]) return null;
+    for (let i = 0; i < this.bucket[index].length; i++) {
+      if (this.bucket[index][i][0] === key) return this.bucket[index][i][1];
     }
     return null;
   }
   has(key) {
     let contains = this.get(key);
-    if(contains === null) return false;
+    if (contains === null) return false;
     return true;
   }
   remove(key) {
-    if(this.get(key) === null) return false;
-        let index = this._hash(key);
-        let chain = this.bucket[index];
-    for(let i = 0; i < chain.length; i++) {
-      if(chain[i][0] === key) {
-      chain.splice(i,1);
-      return true;
+    if (this.get(key) === null) return false;
+    let index = this._hash(key);
+    let chain = this.bucket[index];
+    for (let i = 0; i < chain.length; i++) {
+      if (chain[i][0] === key) {
+        chain.splice(i, 1);
+        return true;
       }
     }
   }
   length() {
     let count = 0;
-    for(let i = 0; i < this.bucket.length; i++) {
+    for (let i = 0; i < this.bucket.length; i++) {
       const chain = this.bucket[i];
-      if(!chain) continue;
-      count += chain.length; 
+      if (!chain) continue;
+      count += chain.length;
     }
     return count;
+  }
+  clear() {
+    this.bucket = new Array(this.bucket.length);
   }
 }
 
@@ -65,6 +68,4 @@ hashTable.set("status", "Debugging");
 hashTable.set("focus", "Performance");
 
 console.log(hashTable);
-
-console.log(hashTable.length());
 
